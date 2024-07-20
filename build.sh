dfe817f1b8b768efda2a252a11d0e5f1f75b92e2
Attila Kun <kun.attila.hun@gmail.com>
2024-07-20 15:38:32 +0200

    use emcc to link

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf dist
 mkdir dist
 emcc -c add.c -o dist/add.o
 zig build-obj main.zig -target wasm32-freestanding -fno-entry --export=doCalc -femit-bin=dist/main.o
-zig wasm-ld --no-entry --export-all dist/main.o dist/add.o -o dist/program.wasm
+emcc dist/main.o dist/add.o -o dist/program.wasm --no-entry -s WASM=1 -s EXPORTED_FUNCTIONS='_doCalc' -s ERROR_ON_UNDEFINED_SYMBOLS=1
 wasm-objdump -x dist/program.wasm
 cp index.html dist/index.html
 cp main.js dist/main.js
